0f5572f167a2ff8ab7c569f7c935a560dd328209
User <user@Users-Mac.PK5001Z>
2020-05-29 11:05:02 -0400

    extracted adapter tests

--- app/src/test/java/com/example/nasaapp/AdapterTest.java
+++ app/src/test/java/com/example/nasaapp/AdapterTest.java
@@ -1,9 +1,6 @@
 package com.example.nasaapp;
 
-import androidx.lifecycle.MutableLiveData;
-
 import com.example.nasaapp.view.adapter.NasaAdapter;
-import com.example.nasaapp.viewmodel.NasaViewModel;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,43 +10,17 @@ import org.mockito.MockitoAnnotations;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotNull;
 
-
-public class ViewModelTest {
-
-
-    private String PLANET_NAME = "MARS";
+public class AdapterTest {
     private int ADAPTER_LIST_SIZE = 10;
-
-    @Mock
-    public NasaViewModel viewModel;
 
     @Mock
     public NasaAdapter nasaAdapter;
 
-
     @Before
-    public void setup(){
+    public void setup() {
         MockitoAnnotations.initMocks(this);
         Mockito.when(nasaAdapter.getItemCount()).thenReturn(ADAPTER_LIST_SIZE);
-        Mockito.when(viewModel.getImageData()).thenReturn(new MutableLiveData<>());
-        Mockito.when(viewModel.getPlanet()).thenReturn(PLANET_NAME);
-    }
-
-    @Test
-    public void test_Incorrect_GetPlanetName_ViewModel(){
-        assertNotEquals(viewModel.getPlanet(), "jupiter");
-    }
-
-    @Test
-    public void test_GetPlanetName_ViewModel(){
-        assertEquals(viewModel.getPlanet(), PLANET_NAME);
-    }
-
-    @Test
-    public void test_getMutableLiveData_ViewModel(){
-        assertNotNull(viewModel.getImageData());
     }
 
     @Test
@@ -61,6 +32,5 @@ public class ViewModelTest {
     public void verifyAdapterListSize_Incorrect_Size(){
         assertNotEquals(nasaAdapter.getItemCount(), 0);
     }
-
 
 }
